3a254ca99dde6d3fd79bd396463ac7e75e60633c
José Escrich <jescrich@gmail.com>
2024-12-06 13:26:44 -0300

    feat: adds get,set,del to cache service

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -36,4 +36,16 @@ export class CacheService implements OnModuleInit {
   async invalidate(key: string) {
     await this.cache.del(key);
   }
+
+  async del(keys: string[]) {
+    await this.cache.del(keys);
+  }
+
+  async get<T>(key: string): Promise<T> {
+    return this.cache.get(key);
+  }
+
+  async set<T>(key: string, value: T, ttl?: number) {
+    await this.cache.set(key, value, ttl ? ttl * 1000 : undefined);
+  }
 }
