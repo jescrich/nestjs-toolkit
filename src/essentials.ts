b64fc835b9ed131db8b7161077041973d4ee4ea8
José Escrich <jescrich@gmail.com>
2024-12-09 22:36:37 -0300

    chore: essentials

--- src/essentials.ts
+++ src/essentials.ts
@@ -1,25 +1,31 @@
-import { Type, DynamicModule, ForwardReference } from "@nestjs/common";
-import { NestJsModule } from "./nestjs/nestjs.module";
-import { CacheModule } from "./cache/cache.module";
-import { KafkaModule } from "./kafka";
+import { Type, DynamicModule, ForwardReference } from '@nestjs/common';
+import { NestJsModule } from './nestjs/nestjs.module';
+import { CacheModule } from './cache/cache.module';
+import { KafkaModule } from './kafka';
 
-const Essentials = (config?: {
+const Essentials = {
+  register: (config?: {
     cache?: {
-        enabled: boolean;
-        host?: string;
-        port?: number;
-    },
+      enabled: boolean;
+      host?: string;
+      port?: number;
+    };
     kafka?: {
-        enabled: boolean;
-        clientId?: string;
-        brokers?: string[];
-    },
-}): Array<Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference> => {
-  return [
-    NestJsModule,
-    config?.cache?.enabled ? { module: CacheModule, redis: { host: config.cache.host, port: config.cache.port } } : null,
-    config?.kafka?.enabled ? { module: KafkaModule, clientId: config.kafka.clientId, brokers: config.kafka.brokers } : null,
-  ].filter(module => module !== null);
+      enabled: boolean;
+      clientId?: string;
+      brokers?: string;
+    };
+  }): Array<Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference> => {
+    return [
+      NestJsModule,
+      config?.cache?.enabled
+        ? { module: CacheModule, redis: { host: config.cache.host, port: config.cache.port } }
+        : null,
+      config?.kafka?.enabled
+        ? { module: KafkaModule, clientId: config.kafka.clientId, brokers: config.kafka.brokers }
+        : null,
+    ].filter((module) => module !== null);
+  },
 };
 
 export { Essentials };
